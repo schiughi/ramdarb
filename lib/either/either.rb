f0625b8c3bca1202778e7e3e25899ac2c404c956
schiughi <schiughi@gmail.com>
2018-06-24 17:53:48 +0900

    add alias

--- lib/either/either.rb
+++ lib/either/either.rb
@@ -22,4 +22,17 @@ class Either
   def initialize(value)
     @value = value
   end
+
+  def left?
+    kind_of? Left
+  end
+
+  def right?
+    kind_of? Right
+  end
+
+  alias_method :ok?, :right?
+  alias_method :success?, :right?
+  alias_method :ng?, :left?
+  alias_method :failure?, :left?
 end
